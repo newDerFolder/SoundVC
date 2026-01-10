80fec116fec7071a259749dfddd75fd2c722f50f
newDerFolder <KLderMR@163.com>
2026-01-10 19:22:34 +0800

    Update SoundVcPlayer2D.gd

--- soundVC/SoundVcPlayer2D.gd
+++ soundVC/SoundVcPlayer2D.gd
@@ -8,8 +8,9 @@ enum PLAY_MODE{RANDOM = 0, SEQUENTIAL = 1, SET_RANDOM = 2, SMART_RANDOM = 3}
 @export var sounds_stream: Array[AudioStream]
 @export var play_mode: PLAY_MODE = PLAY_MODE.SET_RANDOM
 @export var auto_shuffle: bool = true
-#@export_group("SMART_RANDOM_MODE")
-#@export var set_range:int=5
+@export_group("smart_random_mode")
+@export var set_played_range: int = 2
+
 
 
 var play_id = -1
@@ -18,15 +19,21 @@ var played_id_set: Array = []
 func _ready() -> void:
 	match play_mode:
 		PLAY_MODE.SET_RANDOM:
-			shuffle_set()
+			if sounds_stream.size() > 0: # 新增容错：数组为空时不执行洗牌
+				shuffle_set(randi_range(0,sounds_stream.size()-1))
 
-func shuffle_set():
+func shuffle_set(end_id:int):
 	played_id_set.clear()
 	for i in range(sounds_stream.size()):
 		played_id_set.append(i)
 	played_id_set.shuffle()
+	if played_id_set[0]==end_id:
+		var temp=played_id_set[0]
+		var swId=randi_range(1,played_id_set.size()-1)
+		played_id_set[0]=played_id_set[swId]
+		played_id_set[swId]=temp
 
-func play_SoundVc(from_position: float = 0.0) -> void:
+func vc_play_sound(from_position: float = 0.0) -> void:
 	if sounds_stream.size() == 0:
 		return
 	
@@ -43,23 +50,24 @@ func play_SoundVc(from_position: float = 0.0) -> void:
 		PLAY_MODE.SET_RANDOM:
 			play_id += 1
 			if play_id >= played_id_set.size():
-				play_id = 0
 				if auto_shuffle:
-					shuffle_set()
+					shuffle_set(played_id_set[-1])
+				play_id = 0
 			
 			var stream_index = played_id_set[play_id]
 			playVc_stream = sounds_stream[stream_index]
 			
 		PLAY_MODE.SMART_RANDOM:
-			if played_id_set.size() >= sounds_stream.size():
-				played_id_set.clear()
-			
-			var i = randi_range(0, sounds_stream.size() - 1)
+			if set_played_range>sounds_stream.size()-1:
+				set_played_range=sounds_stream.size()-1
+			while played_id_set.size()>set_played_range:
+				played_id_set.pop_front()
+			var i=randi_range(0,sounds_stream.size()-1)
 			while played_id_set.has(i):
-				i = randi_range(0, sounds_stream.size() - 1)
-			
+				i=randi_range(0,sounds_stream.size()-1)
 			played_id_set.append(i)
-			playVc_stream = sounds_stream[i]
+			playVc_stream=sounds_stream[i]
 	
-	stream = playVc_stream
-	play(from_position)
+	if playVc_stream:
+		stream = playVc_stream
+		play(from_position)
